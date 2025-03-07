9696ffd0d844d735d7c0912fd6144ca16612e317
Edoardo <edfl.tunzi@gmail.com>
2025-03-07 19:34:30 +0100

    basic Header

--- src/components/Navbar/MyNavBar.jsx
+++ src/components/Navbar/MyNavBar.jsx
@@ -1,18 +1,30 @@
 import "./MyNavBar.css";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const MyNavBar = () => {
   return (
-    <Navbar bg="black" data-bs-theme="dark">
+    <Navbar className="navbarCustom px-4 py-2" expand="md" bg="black" data-bs-theme="dark">
       <Container fluid>
         <Navbar.Brand href="#home">
-          <img src="/src/assets/logoWhite.png" alt="BariLive logo" style={{ width: 180 }} />
+          <img src="/src/assets/logoBianco.png" alt="BariLive logo" style={{ width: 180 }} />
         </Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">HOME</Nav.Link>
-          <Nav.Link href="#features">EVENTI</Nav.Link>
-          <Nav.Link href="#pricing">ARTISTI</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="me-0">
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }}>
+            <Nav.Link className="text-white me-1" href="#mettilinkhome">
+              HOME
+            </Nav.Link>
+            <Nav.Link className="text-white me-1" href="#mettilinkeventi">
+              EVENTI
+            </Nav.Link>
+            <Nav.Link className="text-white me-1" href="#mettilinkartisti">
+              ARTISTI
+            </Nav.Link>
+          </Nav>
+          <Button className=" mt-4 mt-md-0 ms-auto border-2 rounded-0" variant="outline-light">
+            LOGIN
+          </Button>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
