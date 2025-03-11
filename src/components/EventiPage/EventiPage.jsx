3ab6bf9bf48e6b6c1ae5945896db074f2ff744e3
Edoardo <edfl.tunzi@gmail.com>
2025-03-11 17:03:31 +0100

    loading spinner added on eventi page, fixed bug with viewheight

--- src/components/EventiPage/EventiPage.jsx
+++ src/components/EventiPage/EventiPage.jsx
@@ -3,15 +3,18 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import EventoCard from "../EventoCard/EventoCard";
 import "./EventiPage.css";
 import { Calendar, Calendar2, CalendarDate, GeoAlt, MusicNote, Pin, PinMap, Type } from "react-bootstrap-icons";
+import LoadingSpinner from "../Spinner/LoadingSpinner";
 
 const EventiPage = () => {
   const [eventi, setEventi] = useState([]);
   const [filtro, setFiltro] = useState(""); // Tipo di filtro selezionato
   const [query, setQuery] = useState(""); // Testo inserito dall'utente
   const [isFiltered, setIsFiltered] = useState(false); // Stato per sapere se è applicato un filtro
+  const [loading, setLoading] = useState(true);
 
   // Fetch base per ottenere tutti gli eventi
   const getAllEventi = async () => {
+    setLoading(true);
     try {
       const response = await fetch("http://localhost:8080/eventi");
 
@@ -23,6 +26,8 @@ const EventiPage = () => {
       }
     } catch (error) {
       console.log("Errore: " + error);
+    } finally {
+      setLoading(false); // Quando il fetch è terminato, nasconde lo spinner
     }
   };
 
@@ -37,6 +42,8 @@ const EventiPage = () => {
     } else {
       url = `http://localhost:8080/eventi/${filtro}?${filtro}=${query}`; // endpoint per gli altri filtri
     }
+
+    setLoading(true);
     try {
       const response = await fetch(url);
 
@@ -49,6 +56,8 @@ const EventiPage = () => {
       }
     } catch (error) {
       console.log("Errore: " + error);
+    } finally {
+      setLoading(false); // Quando il fetch è terminato, nasconde lo spinner
     }
   };
 
@@ -65,7 +74,7 @@ const EventiPage = () => {
   }, []);
 
   return (
-    <Container className="mx-auto px-5 px-sm-0">
+    <Container className="mx-auto px-5 px-sm-0 container-vh">
       <h1 className="mt-5">Eventi</h1>
 
       {/* Sezione Bottoni Filtri */}
@@ -109,17 +118,21 @@ const EventiPage = () => {
       )}
 
       {/* Lista Eventi */}
-      <Row className="gy-4 mt-3 border-top border-black border-4">
-        {eventi.length > 0 ? (
-          eventi.map((evento) => (
-            <Col key={evento.id} lg={2} md={3} sm={6}>
-              <EventoCard evento={evento} />
-            </Col>
-          ))
-        ) : (
-          <p className="mt-3 text-uppercase">Nessun evento trovato.</p>
-        )}
-      </Row>
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <Row className="gy-4 mt-3 border-top border-black border-4">
+          {eventi.length > 0 ? (
+            eventi.map((evento) => (
+              <Col key={evento.id} lg={2} md={3} sm={6}>
+                <EventoCard evento={evento} />
+              </Col>
+            ))
+          ) : (
+            <p className="mt-3 text-uppercase">Nessun evento trovato.</p>
+          )}
+        </Row>
+      )}
     </Container>
   );
 };
