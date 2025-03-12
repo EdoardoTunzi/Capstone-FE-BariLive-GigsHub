db806e329ad012e97d72d9d616eccd126933cfdd
Edoardo <edfl.tunzi@gmail.com>
2025-03-12 16:43:23 +0100

    added modal and fetch to change password in profilepage

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Form, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingSpinner from "../Spinner/LoadingSpinner";
 import PartecipazioniGrid from "./PartecipazioniGrid";
-import { MdEmail } from "react-icons/md";
+import { MdEdit, MdEmail } from "react-icons/md";
 import { IoPersonSharp } from "react-icons/io5";
 import { logout } from "../../redux/actions/actions";
 import { useNavigate } from "react-router-dom";
+import "./ProfilePage.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
 
 const ProfilePage = () => {
   const user = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
 
+  /* fetch eventi da partecipazioni */
   const [partecipero, setPartecipero] = useState([]);
   const [miInteressa, setMiInteressa] = useState([]);
   const [partecipato, setPartecipato] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  /* modale password change */
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const handleClosePassword = () => setShowPasswordModal(false);
+  const handleShowPassword = () => setShowPasswordModal(true);
+  const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   /* FetchPartecipazioni */
   const fetchPartecipazioni = async (stato, setState) => {
@@ -50,6 +63,40 @@ const ProfilePage = () => {
     navigate("/", { replace: true });
   };
 
+  /* Gestisce la modifica password */
+  const handlePasswordChange = async () => {
+    //setto su null error e success, per evitare messaggi vecchi
+    setError(null);
+    setSuccess(null);
+
+    try {
+      const response = await fetch("http://localhost:8080/user/me/password", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}` // Se usi JWT
+        },
+        body: JSON.stringify({
+          oldPassword,
+          newPassword
+        })
+      });
+      const textData = await response.text();
+
+      if (!response.ok) {
+        throw new Error(textData || "Errore nel cambio password");
+      }
+
+      //scegli se lanciare anceh un toast success a fondo pagina
+      setSuccess(textData);
+      setOldPassword("");
+      setNewPassword("");
+    } catch (error) {
+      console.log("Errore: ", error);
+      setError(error.message);
+    }
+  };
+
   useEffect(() => {
     setLoading(true);
     Promise.all([
@@ -62,17 +109,22 @@ const ProfilePage = () => {
 
   return (
     <>
-      <Container>
+      <Container className="container-vh">
         {/* Sezione info account */}
         <div className="profile-info my-5">
           <div className="d-flex flex-wrap align-items-center justify-content-between">
             {/* Sezione Avatar + Info */}
-            <div className="d-flex align-items-center">
-              <div
-                className="rounded-circle border border-black overflow-hidden d-flex align-items-center justify-content-center shadow-sm"
-                style={{ width: 150, height: 150 }}
-              >
-                <img className="w-100 h-100 object-fit-cover" src={user.avatar} alt="avatar utente" />
+            <div className="d-flex align-items-center ">
+              <div className="position-relative">
+                <div
+                  className="rounded-circle border border-black overflow-hidden d-flex align-items-center justify-content-center shadow-sm"
+                  style={{ width: 150, height: 150 }}
+                >
+                  <img className="w-100 h-100 object-fit-cover" src={user.avatar} alt="avatar utente" />
+                  <div className="editAvatar position-absolute bottom-0 end-0  bg-white rounded-circle shadow-sm text-black border border-black">
+                    <MdEdit size={20} />
+                  </div>
+                </div>
               </div>
 
               <div className="ms-4">
@@ -88,12 +140,12 @@ const ProfilePage = () => {
               </div>
             </div>
 
-            {/* Sezione Pulsanti */}
+            {/* Sezione Pulsanti azioni profilo, sulla destra*/}
             <div className="d-flex flex-column align-items-end">
               <Button variant="dark" className="mb-2" onClick={handleLogout}>
                 Log out
               </Button>
-              <Button variant="link" className="text-black p-0">
+              <Button variant="link" className="text-black p-0" onClick={handleShowPassword}>
                 Cambia Password
               </Button>
               <Button variant="link" className="text-black p-0">
@@ -117,6 +169,54 @@ const ProfilePage = () => {
           </>
         )}
       </Container>
+
+      {/* MODALI */}
+
+      {/* modale cambio password */}
+      <Modal show={showPasswordModal} onHide={handleClosePassword}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cambia Password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {error && <p className="text-danger">{error}</p>}
+          {success && <p className="text-success">{success}</p>}
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Password attuale</Form.Label>
+              <Form.Control
+                className="shadow-black bs-dark-border-subtle"
+                value={oldPassword}
+                onChange={(e) => setOldPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Nuova password</Form.Label>
+              <Form.Control
+                className="shadow-black bs-dark-border-subtle"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                type={showPassword ? "text" : "password"}
+                required
+              />
+            </Form.Group>
+          </Form>
+          <Button variant="dark" onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClosePassword}>
+            Close
+          </Button>
+          <Button variant="dark" onClick={handlePasswordChange}>
+            Conferma
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* modale cambio avatar */}
     </>
   );
 };
